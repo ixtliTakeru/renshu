e098e38d4ade3c21f3ea64b2a920f3f37b6e73c6
TakeruC <takeru@emq.com>
2016-12-07 15:04:36 +0800

    add AsyncToRxActivity

--- app/src/main/java/g/takeru/renshu/MainActivity.java
+++ app/src/main/java/g/takeru/renshu/MainActivity.java
@@ -1,7 +1,10 @@
 package g.takeru.renshu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import g.takeru.renshu.rx.AsyncToRxActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, AsyncToRxActivity.class));
     }
 }
---
+++ app/src/main/java/g/takeru/renshu/rx/AsyncToRxActivity.java
@@ -0,0 +1,83 @@
+package g.takeru.renshu.rx;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import g.takeru.renshu.R;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func0;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by takeru on 2016/12/7.
+ *
+ * This is a example for convert AsyncTask to RxJava
+ */
+
+public class AsyncToRxActivity extends AppCompatActivity{
+
+    private static final String TAG = AsyncToRxActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        byte[] data = new byte[10];
+        savePhotoObservable2(data)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onNext(String uri) {
+                        // after photo saved, do next step
+                        Log.d(TAG, "uri: " + uri);
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                });
+    }
+
+    // we do more io operation
+    private String savePhoto(byte[] data){
+        Log.d(TAG, "savePhoto");
+        return "uri";
+    }
+
+    // one way: using create
+    private Observable<String> savePhotoObservable(byte[] data){
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext(savePhoto(data));
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    // another way: using defer
+    private Observable<String> savePhotoObservable1(byte[] data){
+        return Observable.defer(new Func0<Observable<String>>() {
+            @Override
+            public Observable<String> call() {
+                return Observable.just(savePhoto(data));
+            }
+        });
+    }
+
+    // using lambda and defer
+    private Observable<String> savePhotoObservable2(byte[] data){
+        return Observable.defer(() -> Observable.just(savePhoto(data)));
+    }
+
+}
