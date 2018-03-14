93b1d0e68cf3d0c317fcae9c94bbd9c9a9a6a48c
TakeruC <takeru@emq.com>
2018-03-14 17:47:14 +0800

    update onClickListener usage in kotlin

--- app/src/main/java/g/takeru/renshu/kotlin/KotlinActivity.kt
+++ app/src/main/java/g/takeru/renshu/kotlin/KotlinActivity.kt
@@ -12,7 +12,7 @@ import timber.log.Timber
  * Created by takeru on 2017/6/13.
  */
 
-class KotlinActivity : AppCompatActivity(){
+class KotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +24,52 @@ class KotlinActivity : AppCompatActivity(){
     private fun bindView() {
         // Basic View Binding
         var textView: TextView?
+        //  for API level 25 and below
         textView = findViewById(R.id.text) as TextView
+        //  for API level 26
+        textView = findViewById(R.id.text)
         textView.setText("Basic View Binding")
 
         // Using Kotlin Android Extensions
         text.setText("Using Kotlin Android Extensions")
 
         // set click listener
-        text.setOnClickListener(onClickListener)
+        text.setOnClickListener({view -> Timber.d("click: " + view.id)})
+        text.setOnClickListener() {view -> Timber.d("click: " + view.id)}
         text.setOnClickListener{view -> Timber.d("click: " + view.id)}
+        text.setOnClickListener(onClickListener1)
+        //text.setOnClickListener(onClickListener2)
+        //text.setOnClickListener(onClickListener3)
+
+        // object copy
+        var takeru = User("takeru", 35)
+        Timber.d(takeru.toString())
+        var takeruCopy = takeru.copy(age=25)
+        Timber.d(takeruCopy.toString())
+
+        // object revert
+        val jane = User("Jane", 35)
+        val (name, age) = jane
+        println("$name, $age years of age")               // prints "Jane, 35 years of age"
+        println("${jane.name}, ${jane.age} years of age") // prints "Jane, 35 years of age"
+        Timber.d(name)
+        Timber.d("$age")
     }
 
-    private val onClickListener = View.OnClickListener {
-        view -> Timber.d("click: " + view.id)
+    // basic onClickListener
+    private var onClickListener1 : View.OnClickListener = object : View.OnClickListener {
+        override fun onClick(view: View) {
+            Timber.d("onClickListener1: " + view.id)
+        }
+    }
+
+    // convent to lambda
+    private val onClickListener2 = View.OnClickListener {
+        view -> Timber.d("onClickListener2: " + view.id)
+    }
+
+    // In kotlin, use a function as a parameter in another function
+    private val onClickListener3 : ((View) -> Unit) = {
+        View -> Timber.d("onClickListener3: " + View.id)
     }
 }
