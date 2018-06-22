35fbca7281991919b5e13cf0e407c1ba18d7a619
TakeruC <takeru@emq.com>
2018-06-22 15:23:42 +0800

    update constants

--- app/src/main/java/g/takeru/renshu/kotlin/Constants.kt
+++ app/src/main/java/g/takeru/renshu/kotlin/Constants.kt
@@ -2,3 +2,34 @@ package g.takeru.renshu.kotlin
 
 class Constants {
 }
+
+enum class Day {
+    Morning, Afternoon, Night;
+
+    override fun toString(): String {
+        return super.toString().toUpperCase()
+    }
+}
+
+enum class Months(var shorthand: String) {
+    January("JAN"){
+        override fun printSomething() {
+            println("First month of the year.")
+        }
+    },
+
+    February("FEB"){
+        override fun printSomething() {
+            println("Second month of the year.")
+        }
+    },
+
+    March("MAR"){
+        override fun printSomething() {
+            println("Third month of the year.")
+        }
+    };
+
+    var hello: String = "Say Hello to Kotlin Enums"
+    abstract fun printSomething()
+}
