7398e25d4fb5a95666cdf44e33efe80af3a2f2e6
TakeruC <takeru@emq.com>
2018-12-13 16:50:24 +0800

    add ripple activity

---
+++ app/src/main/java/g/takeru/renshu/kotlin/animation/RippleActivity.kt
@@ -0,0 +1,13 @@
+package g.takeru.renshu.kotlin.animation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import g.takeru.renshu.R
+
+class RippleActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ripple)
+    }
+}
